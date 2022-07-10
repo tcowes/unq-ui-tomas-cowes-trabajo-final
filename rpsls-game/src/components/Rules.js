3937c3e37c4aaac4ead32a90355f9f09d465a0cc
tcowes <tomascowes97@gmail.com>
2022-07-10 17:59:52 -0300

    Agrego primera imagen ilustrativa a la page de reglas

--- rpsls-game/src/components/Rules.js
+++ rpsls-game/src/components/Rules.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import rulesImg from "../assets/rpsls.png"
 
 const Rules = () => {
     return (
         <div>
-            <Link to={'/'}>
-                <button className="button">Go to home</button>
-            </Link>
+            <img src={rulesImg} height="300px"></img>
+            <div>
+                <Link to={'/'}>
+                    <button className="button">Go to home</button>
+                </Link>
+            </div>
         </div>
     )
 }
